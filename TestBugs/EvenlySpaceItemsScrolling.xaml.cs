66d6e5e34589677aebd8617c5a5024465b7ebb2c
ToolmakerSteve <ToolmakerSteve@users.noreply.github.com>
2021-12-22 18:34:06 -0600

    EvenlySpaceItemsScrolling - override LayoutChildren

--- TestBugs/EvenlySpaceItemsScrolling.xaml.cs
+++ TestBugs/EvenlySpaceItemsScrolling.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 using Xamarin.Essentials;
@@ -18,28 +17,26 @@ namespace TestBugs
 			BindingContext = this;
 		}
 
+		const int NItemsToShow = 5;
+		const int ItemWidth = 30;
+
 		protected override void OnAppearing()
 		{
 			base.OnAppearing();
 
-			const int NItemsToShow = 5;
-			const int ItemWidth = 30;
-			// If the amount of space to left and right of scrolling area is known,
-			// we can handle it before the page is laid out.
-			// See xaml: the width of left column is 44, right column is 44.
-			const int SubtractThisWidth = 44 + 44;
-			Solution1(NItemsToShow, ItemWidth, SubtractThisWidth);
+			if (false)
+			{
+				// If the amount of space to left and right of scrolling area is known,
+				// we can handle it before the page is laid out.
+				// See xaml: the width of left column is 44, right column is 44.
+				const int SubtractThisWidth = 44 + 44;
+				int collectionWidth = (int)ScreenLogicalWidth() - SubtractThisWidth;
+				SetSpacingAndMargin(NItemsToShow, ItemWidth, collectionWidth);
+			}
 		}
 
-
-		/// <summary>
-		/// If the amount of space to left and right of scrolling area is known,
-		/// we can handle it before the page is laid out.
-		/// </summary>
-		/// <exception cref="NotImplementedException"></exception>
-		private void Solution1(int nItemsToShow, int itemWidth, int subtractThisWidth)
+		private void SetSpacingAndMargin(int nItemsToShow, int itemWidth, int collectionWidth)
 		{
-			int collectionWidth = (int)ScreenLogicalWidth() - subtractThisWidth;
 			int widthPerItem = collectionWidth / nItemsToShow;
 			int spacing = Math.Max(0, widthPerItem - itemWidth);
 			int leftMargin = spacing / 2;
@@ -50,6 +47,16 @@ namespace TestBugs
 			// --- Manually test values that work well at a specific screen width. ---
 			//this.MyItemsLayout.ItemSpacing = 30;
 			//this.clv.Margin = new Thickness(15, 0, 0, 0);
+		}
+
+		protected override void LayoutChildren(double x, double y, double width, double height)
+		{
+			var t1 = this.clv.Width;
+			base.LayoutChildren(x, y, width, height);
+
+			// Dynamically calculate based on collection width.
+			int collectionWidth = (int)this.clv.Width;
+			SetSpacingAndMargin(NItemsToShow, ItemWidth, collectionWidth);
 		}
 
 		public static double ScreenLogicalWidth()
